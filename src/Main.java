96966a8fa247f1aef39bea5d248460d3a5a112bb
Luke Rose <psylr5@nottingham.ac.uk>
2017-10-12 00:30:32 +0100

    Implemented 60 second (pseudo-realtime) update function

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,13 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
     static DatabaseHandler dh = new DatabaseHandler();
     static AlphaVantageHandler avh = new AlphaVantageHandler();
+    static boolean initialUpdate = true;
+    static boolean quit = false;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -19,25 +22,31 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
      }
 
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
 //        launch(args);
 
-        //File file = new File("res/historicstocks/" + symbol + ".csv");
-        //StockRecordParser.importDailyMarketData(file, symbol, dh);
 
-            updateStockData();
-    }
-
-    private static void updateStockData(){
         try {
             dh.init("agent","0Y5q0m28pSB9jj2O");
         } catch (Exception e) {}
 
         avh.init("PBATJ7L9N8SNK835");
+
+
+        while(!quit) {
+            try {
+                updateStockData();
+                TimeUnit.SECONDS.sleep(60);
+            } catch (InterruptedException e) {
+            }
+        }
+
+    }
+
+    private static void updateStockData(){
 
         ArrayList<String> stocks = new ArrayList<>();
 
@@ -47,9 +56,20 @@ public class Main extends Application {
             e.printStackTrace();
         }
 
-
         for(String symbol : stocks) {
+            if(initialUpdate)
             new Thread(()-> {
+                File file = new File("res/historicstocks/" + symbol + ".csv");
+                try {
+                    StockRecordParser.importDailyMarketData(file, symbol, dh);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Successfully committed complete Yahoo records of " + symbol + " to the database!");
+
                 ArrayList<String> temp = null;
                 try {
                     temp = avh.submitRequest("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=1min&datatype=csv&outputsize=full&apikey=" + avh.getApiKey());
@@ -63,8 +83,20 @@ public class Main extends Application {
 
                 System.out.println("Successully committed " + symbol + " intraday history to the database!");
             }).start();
+            else
+                new Thread(()-> {
+                    ArrayList<String> temp = null;
+                try {
+                    temp = avh.submitRequest("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=1min&datatype=csv&outputsize=compact&apikey=" + avh.getApiKey());
+                } catch (IOException e) {
+                }
+                    System.out.println("Downloaded " + symbol + " 1 minute update");
+                    StockRecordParser.importIntradayMarketData(temp, symbol, dh);
+
+                }).start();
         }
 
+        if(initialUpdate)
         for(String symbol : stocks) {
             new Thread(()-> {
                 ArrayList<String> temp = null;
@@ -77,6 +109,9 @@ public class Main extends Application {
             StockRecordParser.importDailyMarketData(temp, symbol, dh);
                 System.out.println("Successully committed " + symbol + " full history to the database!");
             }).start();
+
+            if(initialUpdate)
+                initialUpdate = false;
         }
     }
 }

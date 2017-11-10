9dba298784858953208faf969b997ddff8e74f49
Luke Rose <psylr5@nottingham.ac.uk>
2017-11-10 14:34:13 +0000

    Implemented: Historical News Downloader

--- src/NewsAPIHandler.java
+++ src/NewsAPIHandler.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,7 +15,7 @@ public class NewsAPIHandler {
     static final String INTRINIO_API_CALL = "https://api.intrinio.com/news?ticker=";
     static private String INTRINIO_USERNAME;
     static private String INTRINIO_PASSWORD;
-
+    static private final int PAGES = 0, ARTICLES = 1; //Indices for accessing JSON metadata
 
     static public void authenticate(String username, String password){
         INTRINIO_USERNAME = username;
@@ -28,47 +29,101 @@ public class NewsAPIHandler {
         });
     }
 
-    static public void getLatestNews(ArrayList<String> stockList, DatabaseHandler dh) throws IOException {
-        for(String symbol : stockList) {
-            URL url = new URL(INTRINIO_API_CALL + symbol);
+    static public void getNews(String stock, DatabaseHandler dh) throws IOException, SQLException {
+        if(isOverLimit(dh)) return;
 
-            System.out.println("Downloading Latest News for " + symbol + "...");
+        int values[] = getNews(stock,dh,1); //getNews returns count of number of pages + latest news, because querying just for the number of pages is a waste of an API credit
 
-            String doc = null;
-            try (InputStream in = url.openStream()) {
-                Scanner s = new Scanner(in).useDelimiter(("\\A"));
-                doc = s.next();
-            }
+        int pageSize = 100; //TODO: Maybe make this variable if necessary
+        int storedArticles = Integer.parseInt(dh.executeQuery("SELECT COUNT(*) FROM newsarticles WHERE Symbol='" + stock + "';").get(0));
+        int startPage = values[PAGES] - (int) Math.floor(storedArticles / pageSize);
 
-            try {
-                JSONObject obj = new JSONObject(doc);
-                JSONArray arr = obj.getJSONArray("data");
-                String punctuationRemover = "'";
+        int i = startPage;
 
-                for (int i = 0; i < arr.length(); i++) {
-                    JSONObject ob2 = (JSONObject) arr.get(i);
-                    String title = ob2.getString("title").replaceAll(punctuationRemover, "");
-                    String summary = ob2.getString("summary").replaceAll(punctuationRemover, "");
-                    String date = ob2.getString("publication_date").replaceAll(punctuationRemover, "");
-                    String link = ob2.getString("url").replaceAll(punctuationRemover, "");
-                    date = date.split(" ")[0] + " " + date.split(" ")[1];
+        while(i >= 1 && !isOverLimit(dh))
+            getNews(stock, dh, i--);
+    }
 
-                    String data = "'" + symbol + "','" + title + "','" + summary + "','" + date + "','" + link + "'";
+    static public int getCurrentCalls(DatabaseHandler dh) throws SQLException {
+        ArrayList<String> sCalls = dh.executeQuery("SELECT Calls FROM apicalls WHERE Date = CURDATE() AND Name='INTRINIO';");
 
-                    String query = "SELECT * FROM newsarticles WHERE headline = '" + title + "' AND Symbol = '" + symbol + "'";
+        int calls = 0;
 
-                    ArrayList<String> results = dh.executeQuery(query);
+        if(!sCalls.isEmpty())
+            calls = Integer.parseInt(sCalls.get(0));
 
-                    if(results.isEmpty()) {
-                        System.out.println(title);
-                        String command = "INSERT INTO newsarticles (Symbol, Headline,Description,Published,URL) VALUES (" + data + ");";
+        return calls;
+    }
 
-                        try {
-                            dh.executeCommand(command);
-                        }catch (Exception e){ }
-                    }
-                }
-            } catch (Exception e) { e.printStackTrace(); }
+    static public boolean isOverLimit(DatabaseHandler dh, int callsToPerform) throws SQLException {
+        ArrayList<String> sMaxCalls = dh.executeQuery("SELECT DailyLimit FROM apimanagement WHERE Name='INTRINIO';");
+
+        int limit = Integer.parseInt(sMaxCalls.get(0));
+
+        return (callsToPerform + getCurrentCalls(dh)) == limit;
+    }
+
+    static public boolean isOverLimit(DatabaseHandler dh) throws SQLException {
+        return isOverLimit(dh, getCurrentCalls(dh));
+    }
+
+    static public int[] getNews(String stock, DatabaseHandler dh, int page) throws IOException, SQLException {
+        int pages = 0;
+        int articles = 0;
+        URL url = new URL(INTRINIO_API_CALL + stock + "&page_number=" + page);
+
+        if(page == 1)
+            System.out.println("Downloading Latest News for " + stock + "...");
+        else
+            System.out.println("Downloading Historical News (Page " + page + ") for " + stock + "...");
+
+        String doc = null;
+        try (InputStream in = url.openStream()) {
+            Scanner s = new Scanner(in).useDelimiter(("\\A"));
+            doc = s.next();
         }
+
+        dh.executeCommand("INSERT INTO apicalls VALUES ('INTRINIO',CURDATE(), '1') ON UPDATE SET Calls = Calls + 1;");
+
+        try {
+            JSONObject obj = new JSONObject(doc);
+            JSONArray arr = obj.getJSONArray("data");
+            pages = obj.getInt("total_pages");
+            articles = obj.getInt("result_count");
+            String punctuationRemover = "'";
+
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject ob2 = (JSONObject) arr.get(i);
+                String title = ob2.getString("title").replaceAll(punctuationRemover, "");
+                String summary = ob2.getString("summary").replaceAll(punctuationRemover, "");
+                String date = ob2.getString("publication_date").replaceAll(punctuationRemover, "");
+                String link = ob2.getString("url").replaceAll(punctuationRemover, "");
+                date = date.split(" ")[0] + " " + date.split(" ")[1];
+
+                String data = "'" + stock + "','" + title + "','" + summary + "','" + date + "','" + link + "'";
+
+                String query = "SELECT * FROM newsarticles WHERE headline = '" + title + "' AND Symbol = '" + stock + "'";
+
+                ArrayList<String> results = dh.executeQuery(query);
+
+                if(results.isEmpty()) {
+                    System.out.println(title);
+                    String command = "INSERT INTO newsarticles (Symbol, Headline,Description,Published,URL) VALUES (" + data + ");";
+
+                    try {
+                        dh.executeCommand(command);
+                    }catch (Exception e){ }
+                }
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+
+        int[] values = {pages,articles};
+
+        return values;
+    }
+
+    static public void getNews(ArrayList<String> stockList, DatabaseHandler dh) throws IOException, SQLException {
+        for(String symbol : stockList)
+            getNews(symbol,dh);
     }
 }

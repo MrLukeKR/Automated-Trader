3058988b8b39647cc3409824dfa0416f3dabbd62
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-02-01 00:13:19 +0000

    Refactored: SRP is now initialised with a database handler rather than requiring it on each function call

--- src/StockRecordParser.java
+++ src/StockRecordParser.java
@@ -7,8 +7,16 @@ import java.util.ArrayList;
 
 public class StockRecordParser {
     static final String IS_NUMERIC = "[-+]?\\d*\\.?\\d+";
+    static DatabaseHandler dh;
 
-    static public void importDailyMarketData(File csvFile, String symbol, DatabaseHandler dh) throws IOException {
+
+    static public void initialise(DatabaseHandler sqdh) {
+        dh = sqdh;
+
+        System.out.println("Initialised Stock Record Parser");
+    }
+
+    static public void importDailyMarketData(File csvFile, String symbol) throws IOException {
         FileReader fr = new FileReader(csvFile);
         BufferedReader br = new BufferedReader(fr);
 
@@ -18,66 +26,51 @@ public class StockRecordParser {
         while((curr = br.readLine()) != null)
             csvArray.add(curr);
 
-        importDailyMarketData(csvArray, symbol, dh);
+        importDailyMarketData(csvArray, symbol);
     }
 
-    static public void importDailyMarketData(ArrayList<String> csv, String symbol, DatabaseHandler dh) {
+    static public void importDailyMarketData(ArrayList<String> csv, String symbol) {
         if (csv == null || csv.isEmpty()) return;
 
-        for(String curr : csv) {
-            if (curr != null) {
-                String[] split = curr.split(",");
-                if (split.length == 6 && split[1].matches(IS_NUMERIC)) {
-                    String statement = "INSERT INTO dailystockprices (Symbol, TradeDate, OpenPrice, HighPrice, LowPrice, ClosePrice, TradeVolume) VALUES(";
+        importData(csv, "(Symbol, TradeDate, OpenPrice, HighPrice, LowPrice, ClosePrice, TradeVolume)", "dailystockprices", symbol, 6);
 
-                    statement += "'" + symbol + "','" + split[0] + "'" + curr.replaceAll(split[0],"")
-                            + ") ON DUPLICATE KEY UPDATE " +
-                            "OpenPrice = '" + split[1] +
-                            "', HighPrice = '" + split[2] +
-                            "', LowPrice = '" + split[3] +
-                            "', ClosePrice = '" + split[4] +
-                            "', TradeVolume = '" + split[5] + "';";
-
-                    try {
-                        dh.executeCommand(statement);
-                    } catch (Exception e) {
-                        System.err.println(e.getMessage() + " " + statement);
-                    }
-                }
-            }
-        }
     }
 
-    static public void importData(ArrayList<String> csv, String columns, String table, String symbol, DatabaseHandler dh, int columnCount)  {
+    static public void importIntradayMarketData(ArrayList<String> csv, String symbol) {
         if (csv == null || csv.isEmpty()) return;
 
+        importData(csv, "(Symbol, TradeDateTime, OpenPrice, HighPrice, LowPrice, ClosePrice, TradeVolume)", "intradaystockprices", symbol, 6);
+    }
+
+    static public void importData(ArrayList<String> csv, String columns, String table, String symbol, int columnCount) {
+        if (csv == null || csv.isEmpty()) return;
+
+        String statement = "INSERT INTO " + table + columns + " VALUES";
+
+        int count = 1;
         for(String curr : csv) {
             if (curr != null) {
                 String split[] = curr.split(",");
                 if (split.length == columnCount && split[1].matches(IS_NUMERIC)) {
-                    String statement = "INSERT INTO " + table + columns + " VALUES(";
 
-                    statement += "'" + symbol + "','" + split[0] + "'" + curr.replaceAll(split[0], "") + ")" +
-                            " ON DUPLICATE KEY UPDATE " +
-                            "OpenPrice = '" + split[1] +
-                            "', HighPrice = '" + split[2] +
-                            "', LowPrice = '" + split[3] +
-                            "', ClosePrice = '" + split[4] +
-                            "', TradeVolume = '" + split[5] + "';";
-
-                    try {
-                        dh.executeCommand(statement);
-                    } catch (Exception e) {
-                        System.err.println(e.getMessage() + " " + statement);
-                    }
+                    statement += "('" + symbol + "','" + split[0] + "'" + curr.replaceAll(split[0], "") + ")";
+                    if (++count < csv.size())
+                        statement += ",\r\n";
                 }
             }
         }
-    }
 
-    static public void importIntradayMarketData(ArrayList<String> csv, String symbol, DatabaseHandler dh) {
-        if(csv == null || csv.isEmpty()) return;
+        statement += " ON DUPLICATE KEY UPDATE " +
+                "OpenPrice = VALUES(OpenPrice)" +
+                ", HighPrice = VALUES(HighPrice)" +
+                ", LowPrice = VALUES(LowPrice)" +
+                ", ClosePrice = VALUES(ClosePrice)" +
+                ", TradeVolume = VALUES(TradeVolume);";
 
-        importData(csv, "(Symbol, TradeDateTime, OpenPrice, HighPrice, LowPrice, ClosePrice, TradeVolume)","intradaystockprices", symbol, dh, 6);
+        try {
+            dh.executeCommand(statement);
+        } catch (Exception e) {
+            System.err.println(e.getMessage() + " " + statement);
+        }
     }
 }

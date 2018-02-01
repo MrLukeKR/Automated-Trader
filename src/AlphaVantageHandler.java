db7b3ba0e742b7f8d24cb1f16a87c48800670a64
Luke Rose <psylr5@nottingham.ac.uk>
2018-02-01 00:04:50 +0000

    Fixed: Errors now cause an exponential backoff and a timeout of 10 attempts before continuing with the next request

--- src/AlphaVantageHandler.java
+++ src/AlphaVantageHandler.java
@@ -8,29 +8,39 @@ import java.util.concurrent.TimeUnit;
 public class AlphaVantageHandler {
     private String apiKey;
 
+    private static final int WAIT_PERIOD = 2; //This is used to prevent the server rejecting requests from excessive usage
+
     public void init(String apiKey){
         this.apiKey = apiKey;
     }
 
     public ArrayList<String> submitRequest(String request) throws IOException, InterruptedException {
-        int exceeded = 0;
+        int exceeded = 1;
         ArrayList<String> temp = new ArrayList<>();
 
         do {
-        URL url = new URL(request);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            URL url = new URL(request);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            //TODO: 503 Error handling
 
-        //TODO: Ensure the download is successful
-        String sTemp;
+            String sTemp;
 
             while ((sTemp = reader.readLine()) != null)
-                if (sTemp.contains("Please consider optimizing your API call frequency."))
-                    TimeUnit.SECONDS.sleep(++exceeded);
-                else {
+                if (sTemp.contains("Please consider optimizing your API call frequency.") || sTemp.contains("}") || sTemp.contains("{")) {
+                    TimeUnit.SECONDS.sleep(exceeded++);
+                } else if (sTemp.contains("Invalid API call")) {
+                    System.err.println("Possible invalid API call: " + request);
+                    TimeUnit.SECONDS.sleep(exceeded++);
+                } else {
                     temp.add(sTemp);
-                    if (exceeded != 0) exceeded = 0;
+                    if (exceeded != 1) exceeded = 1;
                 }
-        } while (exceeded > 0);
+        } while (exceeded > 1 && exceeded < 10);
+
+        if (temp.size() <= 2)
+            System.err.println("Error with quote download");
+
+        //TimeUnit.SECONDS.sleep(WAIT_PERIOD);
 
         return temp;
     }

cfbb884c00762debe231dd584214757df9265412
Luke <lrose96@outlook.com>
2018-03-20 11:36:09 +0000

    Refactored: To allow for safer sorting and access, prices are now linked with their corresponding dates

--- src/main/java/Portfolio/PortfolioManager.java
+++ src/main/java/Portfolio/PortfolioManager.java
@@ -2,6 +2,7 @@ package Portfolio;
 
 import Default.DatabaseHandler;
 
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -15,20 +16,20 @@ public class PortfolioManager {
         System.out.println("Initialised Portfolio Manager");
     }
 
-    static public ArrayList<Double> getPrices(String symbol, int limit) throws SQLException {
-        ArrayList<Double> prices = new ArrayList<>();
+    static public TreeMap<Date, Double> getPrices(String symbol, int limit) throws SQLException {
+        TreeMap<Date, Double> prices = new TreeMap<>();
 
-        for (String record : dh.executeQuery("SELECT ClosePrice FROM dailystockprices WHERE Symbol='" + symbol + "' AND ClosePrice is not null AND ClosePrice != 0 ORDER BY TradeDate DESC LIMIT " + limit))
-            prices.add(Double.valueOf(record));
-
-        Collections.reverse(prices);
+        for (String record : dh.executeQuery("SELECT TradeDate, ClosePrice FROM dailystockprices WHERE Symbol='" + symbol + "' AND ClosePrice is not null AND ClosePrice != 0 ORDER BY TradeDate DESC LIMIT " + limit)) {
+            String[] splitRecord = record.split(",");
+            prices.put(Date.valueOf(splitRecord[0]),Double.valueOf(splitRecord[1]));
+        }
 
         return prices;
     }
 
     private static double calculateCovariance(ArrayList<Double> stock1Returns, ArrayList<Double> stock2Returns) {
         if (stock1Returns.size() != stock2Returns.size())
-            throw new Error(); //TODO: Customise this error
+            throw new Error();
 
         double total = 0;
 
@@ -36,7 +37,7 @@ public class PortfolioManager {
         double average2 = calculateAverage(stock2Returns);
 
         for (int i = 0; i < stock1Returns.size(); i++)
-            total += (stock1Returns.get(i) - average1) * (stock2Returns.get(i) - average2);
+            total += (stock1Returns.get(i) - average1) * (stock2Returns.get(i) - average2) ;
 
         return total / (stock1Returns.size() - 1); // -1 because this is sample based, not population based
     }
@@ -51,11 +52,12 @@ public class PortfolioManager {
         return total / (returns.size() - 1); // -1 because this is sample based, not population based
     }
 
-    private static ArrayList<Double> calculateReturns(ArrayList<Double> prices, int amountOfDays) {
+    private static ArrayList<Double> calculateReturns(TreeMap<Date, Double> prices, int amountOfDays) {
         ArrayList<Double> returns = new ArrayList<>();
+        ArrayList<Date> dates = new ArrayList<>(prices.keySet());
 
-        for (int i = 0; i < prices.size() - amountOfDays; i++)
-            returns.add(Math.log(prices.get(i + amountOfDays) / prices.get(i))); //Logarithmic Return
+        for (int i = 0; i < dates.size() - amountOfDays; i++)
+            returns.add(Math.log(prices.get(dates.get(i + amountOfDays)) / prices.get(dates.get(i)))); //Logarithmic Return
         //returns.add((prices.get(i + amountOfDays) / prices.get(i)) -1); //Arithmetic Return
 
         return returns;
@@ -68,39 +70,55 @@ public class PortfolioManager {
         return sum / values.size();
     }
 
-    private static double[][] calculateCovarianceMatrix(ArrayList<ArrayList<Double>> returns) {
-        double[][] covarianceMatrix = new double[returns.size()][returns.size()];
+    private static double[][] calculateCovarianceMatrix(ArrayList<Stock> stocksToCalculate) {
+        double[][] covarianceMatrix = new double[stocksToCalculate.size()][stocksToCalculate.size()];
 
-        for (int i = 0; i < returns.size(); i++)
-            for (int j = 0; j < returns.size(); j++)
-                covarianceMatrix[i][j] = calculateCovariance(returns.get(i), returns.get(j));
+        for(Stock stock1 : stocksToCalculate)
+            for (Stock stock2 : stocksToCalculate)
+                covarianceMatrix[stocksToCalculate.indexOf(stock1)][stocksToCalculate.indexOf(stock2)] = calculateCovariance(stock1.getReturns(), stock2.getReturns());
 
         return covarianceMatrix;
     }
 
-    static public Map<String, Double> optimisePortfolio(OptimisationMethod method, int holdPeriod, TreeMap<String, ArrayList<Double>> prices, boolean showDebug) throws SQLException {
+    static public Map<String, Double> optimisePortfolio(OptimisationMethod method, EvaluationMethod em, int holdPeriod, TreeMap<String, TreeMap<Date, Double>> prices, boolean showDebug) throws SQLException {
         ArrayList<String> stocks = dh.executeQuery("SELECT Symbol FROM indices ORDER BY Symbol ASC;");
-        ArrayList<ArrayList<Double>> returns = new ArrayList<>();
+        ArrayList<Stock> calcStocks = new ArrayList<>();
+
+        TreeMap<String, ArrayList<Double>> returns = new TreeMap<>();
         double[] expectedReturns = new double[stocks.size()];
 
         int i = 0;
 
         for (String stock : stocks) {
-            ArrayList<Double> currentReturns = calculateReturns(prices.get(stock), holdPeriod);
-            returns.add(currentReturns);
-            expectedReturns[i++] = calculateAverage(currentReturns);
+            Stock currStock = new Stock(stock);
+            currStock.setPrices(prices.get(stock));
+            currStock.setReturns(calculateReturns(prices.get(stock), holdPeriod));
+            returns.put(stock, currStock.getReturns());
+            currStock.setExpectedReturn(calculateAverage(currStock.getReturns()));
+            currStock.setReturnsVariance(calculateVariance(currStock.getReturns()));
+            currStock.setReturnsStandardDeviation(Math.sqrt(currStock.getReturnsVariance()));
+
+            calcStocks.add(currStock);
         }
 
-        double[][] covarianceMatrix = calculateCovarianceMatrix(returns);
+        Collections.sort(calcStocks, Comparator.comparing(Stock::getName));
+
+        for(Stock currStock : calcStocks)
+            expectedReturns[i++] = currStock.getExpectedReturn();
+
+        double[][] covarianceMatrix = calculateCovarianceMatrix(calcStocks);
         double[] best = null;
 
         switch(method) {
             case GENETIC_ALGORITHM:
-                best = GAOptimiser.optimise(stocks.size(), 1000, 500, expectedReturns, covarianceMatrix, showDebug);
-            break;
+                best = GAOptimiser.optimise(stocks.size(), em, 1000, 500, expectedReturns, covarianceMatrix, showDebug);
+                break;
             case SIMULATED_ANNEALING:
-            best = SAOptimiser.optimise(stocks.size(), 1, 0.0001, 0.99, 10, expectedReturns, covarianceMatrix, showDebug);
-            break;
+                best = SAOptimiser.optimise(stocks.size(), em, 1, 0.0001, 0.99, 100, expectedReturns, covarianceMatrix, showDebug);
+                break;
+            case DETERMINISTIC:
+                best = DeterministicOptimiser.optimise(stocks.size(), em, expectedReturns, covarianceMatrix);
+                break;
         }
 
         Map<String, Double> portfolio = new HashMap<>();
@@ -114,7 +132,7 @@ public class PortfolioManager {
                 portfolio.put(stocks.get(j), percentage / 100.0);
 
             if(showDebug)
-            System.out.println(stocks.get(j) + " weight: " + percentage + " (Expected Return: " + Math.round(expectedReturns[j] * 10000.0) / 100.0 + "\tRisk: " + calculateVariance(returns.get(j)) + "\tRatio: " + expectedReturns[j] / calculateVariance(returns.get(j)) + ")");
+                System.out.println(stocks.get(j) + " weight: " + percentage + " (Expected Return: " + (Math.round(expectedReturns[j] * 10000.0) / 100.0) + "\tRisk: " + (calculateVariance(returns.get(stocks.get(j))) + "\tRatio: " + (expectedReturns[j] / calculateVariance(returns.get(stocks.get(j)))) + ")"));
         }
 
         double expectedReturn = EvaluationFunction.getReturn(best, expectedReturns);
@@ -134,5 +152,6 @@ public class PortfolioManager {
         return portfolio;
     }
 
-    public enum OptimisationMethod {SIMULATED_ANNEALING, GENETIC_ALGORITHM}
+    public enum OptimisationMethod {SIMULATED_ANNEALING, GENETIC_ALGORITHM, DETERMINISTIC}
+    public enum EvaluationMethod {MAXIMISE_RETURN, MAXIMISE_RETURN_MINIMISE_RISK}
 }

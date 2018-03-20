04991167c3c77d32581afc607bbc38774d023d05
Luke <lrose96@outlook.com>
2018-03-20 11:39:55 +0000

    Implemented: Equal weights can now be used for initialising a weight vector

--- src/main/java/Utility/PortfolioUtils.java
+++ src/main/java/Utility/PortfolioUtils.java
@@ -8,7 +8,6 @@ public class PortfolioUtils {
     static private final Random rng = new Random();
 
     static public double[] getRandomWeights(int amount) {
-
         double[] weights = new double[amount];
 
         for (int i = 0; i < amount; i++)
@@ -17,24 +16,39 @@ public class PortfolioUtils {
         return scaleWeights(weights);
     }
 
+    static public double[] getEqualWeights(int amount) {
+        double[] weights = new double[amount];
+
+        for (int i = 0; i < amount; i++)
+            weights[i] = 1.0 / amount;
+
+        return weights;
+    }
+
     static public double[] mutate(double[] weights, double rate) {
-        for (int i = 0; i < weights.length; i++) {
+        double[] newWeights = new double[weights.length];
+        for (int i = 0; i < newWeights.length; i++) {
             if (Math.random() < rate)
-                weights[i] += rng.nextGaussian();
-            if (weights[i] < 0) weights[i] = 0;
+                newWeights[i] = weights[i] + rng.nextGaussian();
+            if (newWeights[i] < 0) newWeights[i] = 0;
         }
 
-        return scaleWeights(weights);
+        return scaleWeights(newWeights);
     }
 
     static private double[] scaleWeights(double[] originalWeights) {
         double sum = DoubleStream.of(originalWeights).parallel().sum();
 
-        originalWeights = Arrays.stream(originalWeights).map(v -> v /= sum).toArray();
 
-        if(Arrays.stream(originalWeights).sum() < 0.99 || Arrays.stream(originalWeights).sum() > 1.01)
+        double[] newWeights = new double[originalWeights.length];
+
+        for(int i = 0; i < newWeights.length; i++)
+            newWeights[i] = originalWeights[i] / sum;
+
+        if(Arrays.stream(newWeights).sum() < 0.99 || Arrays.stream(newWeights).sum() > 1.01)
             System.err.println("WEIGHT ERROR: " + Arrays.stream(originalWeights).sum());
 
-        return originalWeights;
+
+        return newWeights.clone();
     }
 }

ce2f0f9b80dd88c3199db34efdf0f5d00d20ed48
Luke Rose <psylr5@nottingham.ac.uk>
2018-04-05 14:45:47 +0100

    Documentation: Added JacaDocs for Main class

--- src/main/java/Default/Main.java
+++ src/main/java/Default/Main.java
@@ -8,19 +8,51 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * @author Luke K. Rose <psylr5@nottingham.ac.uk>
+ * @version 1.0
+ * @since 0.1
+ */
 
 public class Main extends Application {
-    static FXMLLoader loader;
-    static private String[] arguments;
+    private static FXMLLoader loader;
+    private static String[] arguments;
 
+    /**
+     * Starts the application and initialises Hadoop utils if on a Windows machine
+     *
+     * @param args Application arguments, passed in via the Command Line or IDE parameters
+     */
     public static void main(String[] args) {
         if (System.getProperty("os.name").toLowerCase().contains("win"))
             System.setProperty("hadoop.home.dir", "c:/hadoop");
         launch(arguments = args);
     }
 
-    public static String[] getArguments(){return arguments;}
+    /**
+     * Accesses the arguments passed into the application
+     *
+     * @return Application arguments
+     */
+    static String[] getArguments() {
+        return arguments;
+    }
 
+    /**
+     * Access the controller used by the JavaFX application
+     *
+     * @return Main controller of the application
+     */
+    static public Controller getController() {
+        return loader.getController();
+    }
+
+    /**
+     * Starts the JavaFX application
+     *
+     * @param primaryStage Stage that contains the JavaFX GUI
+     * @throws IOException Throws IOException if the view.fxml file does not exist/cannot be found or is otherwise unreadable
+     */
     @Override
     public void start(Stage primaryStage) throws IOException {
         loader = new FXMLLoader(getClass().getResource("/view.fxml"));
@@ -37,9 +69,5 @@ public class Main extends Application {
             }
             System.exit(1);
         });
-    }
-
-    static public Controller getController(){
-        return loader.getController();
     }
 }

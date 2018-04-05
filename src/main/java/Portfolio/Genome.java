8404c0877de21d10cae654a237aaa744c3c55009
Luke Rose <psylr5@nottingham.ac.uk>
2018-04-05 21:23:05 +0100

    Documentation: Added JavaDocs for Genome class

--- src/main/java/Portfolio/Genome.java
+++ src/main/java/Portfolio/Genome.java
@@ -1,24 +1,68 @@
 package Portfolio;
 
-public class Genome {
-    double[] genes;
-    Double fitness;
+/**
+ * @author Luke K. Rose <psylr5@nottingham.ac.uk>
+ * @version 1.0
+ * @since 0.4
+ */
 
-    public Genome(double[] genes){ this.genes = genes.clone(); }
+class Genome {
+    private double[] genes;
+    private Double fitness;
 
-    public void setGene(int index, double gene){genes[index] = gene; }
-    public void setGenes(double[] genes){this.genes = genes.clone();}
-    public void setFitness(double fitness){this.fitness = fitness;}
+    /**
+     * Initialises the Genome with an array of gene values
+     *
+     * @param genes Double array of asset weight values
+     */
+    Genome(double[] genes) {
+        this.genes = genes.clone();
+    }
 
-    public double[] getGenes(){return genes;}
-    public double getGene(int index){return genes[index];}
-    public Double getFitness() {return fitness;}
+    /**
+     * Replaces a gene (asset weight) with a given value
+     *
+     * @param index Index of gene to replace
+     * @param gene  Value of gene to use as replacement
+     */
+    void setGene(int index, double gene) {
+        genes[index] = gene;
+    }
 
-    public boolean containsNaN(){
-        for (Double gene : genes)
-            if(Double.isNaN(gene))
-                return true;
+    /**
+     * Access the asset weighting array
+     *
+     * @return Gene array, representing the weights of assets in a portfolio
+     */
+    double[] getGenes() {
+        return genes;
+    }
 
-        return false;
+    /**
+     * Access a single gene value from the gene array
+     *
+     * @param index Index of the gene to retrieve the value from
+     * @return An asset weight (gene)
+     */
+    double getGene(int index) {
+        return genes[index];
+    }
+
+    /**
+     * Access the fitness of this Genome
+     *
+     * @return The performance measurement of the Genome
+     */
+    Double getFitness() {
+        return fitness;
+    }
+
+    /**
+     * Sets the performance measure of this Genome
+     *
+     * @param fitness Performance measurement value
+     */
+    void setFitness(double fitness) {
+        this.fitness = fitness;
     }
 }

0c1a6f20dfed36ec589c1a67fdf81846e78c621c
Luke Rose <psylr5@nottingham.ac.uk>
2018-04-06 01:47:11 +0100

    Documentation: Added JavaDocs for PortfolioUtils class

--- src/main/java/Utility/PortfolioUtils.java
+++ src/main/java/Utility/PortfolioUtils.java
@@ -1,54 +1,78 @@
 package Utility;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.DoubleStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author Luke K. Rose <psylr5@nottingham.ac.uk>
+ * @version 1.0
+ * @since 0.6
+ */
 
 public class PortfolioUtils {
-    static private final Random rng = new Random();
+    /**
+     * Creates a collection of random asset weights
+     *
+     * @param stocks Stocks to create random weights for
+     * @return A Map of Stock-to-Weight values
+     */
+    static public Map<String, Double> getRandomWeights(ArrayList<String> stocks) {
+        Map<String, Double> weights = new TreeMap<>();
 
-    static public double[] getRandomWeights(int amount) {
-        double[] weights = new double[amount];
-
-        for (int i = 0; i < amount; i++)
-            weights[i] = Math.random();
+        for (String stock : stocks)
+            weights.put(stock, Math.random());
 
         return scaleWeights(weights);
     }
 
-    static public double[] getEqualWeights(int amount) {
-        double[] weights = new double[amount];
+    /**
+     * Creates a collection of equal asset weights
+     *
+     * @param stocks Stocks to create equal weights for
+     * @return A Map of Stock-to-Weight values
+     */
+    static public Map<String, Double> getEqualWeights(ArrayList<String> stocks) {
+        Map<String, Double> weights = new TreeMap<>();
 
-        for (int i = 0; i < amount; i++)
-            weights[i] = 1.0 / amount;
+        for (String stock : stocks)
+            weights.put(stock, 1.0 / stocks.size());
 
         return weights;
     }
 
-    static public double[] mutate(double[] weights, double rate) {
-        double[] newWeights = new double[weights.length];
-        for (int i = 0; i < newWeights.length; i++) {
+    /**
+     * Alters a set of weights away from its original value in order to create a novel solution
+     *
+     * @param weights Original weight values that are to be mutated
+     * @param rate    The probability that a gene will be mutated
+     * @return Mutated collection of Stock-to-Weight values
+     */
+    static public Map<String, Double> mutate(Map<String, Double> weights, double rate) {
+        Map<String, Double> newWeights = new TreeMap<>(weights);
+        for (String stock : newWeights.keySet()) {
             if (Math.random() < rate)
-                newWeights[i] = weights[i] + rng.nextGaussian();
-            if (newWeights[i] < 0) newWeights[i] = 0;
+                newWeights.put(stock, newWeights.get(stock));
+            if (newWeights.get(stock) < 0) newWeights.put(stock, 0.0);
         }
 
         return scaleWeights(newWeights);
     }
 
-    static private double[] scaleWeights(double[] originalWeights) {
-        double sum = DoubleStream.of(originalWeights).parallel().sum();
+    /**
+     * Scales the weights to sum to 1 (i.e. so that the "sum to one" constraint cannot be violated)
+     *
+     * @param originalWeights Original asset weightings that are to be scaled
+     * @return Scaled weights that conform to the "sum to one" constraint
+     */
+    static private Map<String, Double> scaleWeights(Map<String, Double> originalWeights) {
+        double sum = 0;
+        for (String stock : originalWeights.keySet()) sum += originalWeights.get(stock);
 
+        Map<String, Double> newWeights = new TreeMap<>(originalWeights);
 
-        double[] newWeights = new double[originalWeights.length];
+        for (String stock : newWeights.keySet()) newWeights.put(stock, originalWeights.get(stock) / sum);
 
-        for(int i = 0; i < newWeights.length; i++)
-            newWeights[i] = originalWeights[i] / sum;
-
-        if(Arrays.stream(newWeights).sum() < 0.99 || Arrays.stream(newWeights).sum() > 1.01)
-            System.err.println("WEIGHT ERROR: " + Arrays.stream(originalWeights).sum());
-
-
-        return newWeights.clone();
+        return newWeights;
     }
 }

3622aecba75446c16931a5edf2f5148718df19dc
Luke Rose <psylr5@nottingham.ac.uk>
2018-04-06 09:46:35 +0100

    Documentation: Added JavaDocs for PredictionBox class

--- src/main/java/Records/PredictionBox.java
+++ src/main/java/Records/PredictionBox.java
@@ -7,10 +7,23 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
+/**
+ * @author Luke K. Rose <psylr5@nottingham.ac.uk>
+ * @version 1.0
+ * @since 0.5
+ */
+
 public class PredictionBox {
     private final HBox predictions = new HBox();
-    Label[] predictionLabels;
+    private Label[] predictionLabels;
 
+    /**
+     * Initialises a new Prediction Box, given a stock, collection of days to predict and their respective prediction values
+     *
+     * @param stock            Stock ticker that the predictions are associated with
+     * @param days             Prediction periods
+     * @param predictionValues Predictions of Rise/Maintain or Fall for each of the prediction periods
+     */
     public PredictionBox(String stock, int[] days, boolean[] predictionValues) {
         predictions.setMinSize(610,20);
         predictions.setPrefSize(610,20);
@@ -19,7 +32,6 @@ public class PredictionBox {
         Label predictionSymbol = new Label();
         predictionSymbol.setFont(Font.font(null, FontWeight.BOLD, 14));
         predictionSymbol.setMinSize(80,20);
-
         predictionSymbol.setText(stock);
 
         predictionLabels = new Label[days.length];
@@ -36,6 +48,10 @@ public class PredictionBox {
         predictions.getChildren().addAll(predictionLabels);
     }
 
+    /**
+     * Updates the prediction values of the record without having to recreate a new PredictionBox
+     * @param predictionValues Boolean array of new prediction values (True = Rise/Maintain, False = Fall)
+     */
     public void updateValues(boolean[] predictionValues){
         for(int i = 0; i < predictionValues.length; i++) {
             if (predictionValues[i]) {
@@ -48,6 +64,10 @@ public class PredictionBox {
         }
     }
 
+    /**
+     * Gets the JavaFX GUI node associated with this record for inclusion with the main application GUI
+     * @return Record's JavaFX GUI node
+     */
     public Node getNode() {
         return predictions;
     }

14e37b0b9d591e532ce76d109e2274aac6aae13f
Luke Rose <psylr5@nottingham.ac.uk>
2018-04-06 10:14:38 +0100

    Documentation: Added JavaDocs for TrainingFileUtils class

--- src/main/java/Utility/TrainingFileUtils.java
+++ src/main/java/Utility/TrainingFileUtils.java
@@ -10,130 +10,69 @@ import javafx.scene.control.ProgressBar;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.TreeMap;
+
+/**
+ * @author Luke K. Rose <psylr5@nottingham.ac.uk>
+ * @version 1.0
+ * @since 0.6
+ */
 
 public class TrainingFileUtils {
     static private DatabaseHandler databaseHandler;
 
+    /**
+     * Initialses the TrainingFileUtils class with a database handler to prevent deadlocks when accessing the database
+     *
+     * @param tfudh Training File Utils Database Handler
+     */
     static public void setDatabaseHandler(DatabaseHandler tfudh) {
         databaseHandler = tfudh;
     }
 
-    static public void splitClassificationLibSVM(String path, double splitPoint, String trainingPath, String testingPath) throws IOException {
-        FileReader fr = new FileReader(path);
-        BufferedReader br = new BufferedReader(fr);
-
-        File trainingFile = new File(trainingPath);
-        File testingFile = new File(testingPath);
-        PrintWriter trainingWriter = new PrintWriter(trainingFile);
-        PrintWriter testingWriter = new PrintWriter(testingFile);
-
-        String line;
-
-        int noOfStocks = 0;
-        int noOfDays = 0;
-
-        int prevStock = -1, currStock = 0;
-        int prevDays = -1, currDays = 0;
-
-        TreeMap<Integer, Integer> stockRecords = new TreeMap<>();
-
-        while ((line = br.readLine()) != null){
-            String[] splitString = line.split(" ");
-            splitString.clone();
-            currStock = Integer.parseInt(splitString[1].split(":")[1]);
-
-            Integer stockCount = stockRecords.get(currStock);
-
-            if(stockCount == null)
-                stockRecords.put(currStock, 0);
-            else
-                stockRecords.put(currStock, stockCount + 1);
-
-            currDays = Integer.parseInt(splitString[2].split(":")[1]);
-            if(currStock != prevStock)
-                noOfStocks++;
-            if(noOfStocks == 1 && currDays != prevDays)
-                noOfDays++;
-            prevStock = currStock;
-            prevDays = currDays;
-        }
-
-        br.reset();
-
-        int trainingAmount = (int) Math.floor(stockRecords.get(0) * splitPoint);
-        int testingAmount = stockRecords.get(0) - trainingAmount;
-
-        int counter = 0;
-        int completed = 0;
-
-        if((trainingAmount + testingAmount) != stockRecords.get(0))
-            System.err.println("Split error!");
-
-        boolean saveToTraining = true;
-
-        while ((line = br.readLine()) != null){
-            if(saveToTraining && counter == trainingAmount) {
-                saveToTraining = false;
-                counter = 0;
-            }
-
-            if(!saveToTraining && counter == testingAmount) {
-                saveToTraining = true;
-                counter = 0;
-                // trainingAmount = (int) Math.floor(stock)
-            }
-
-            if(saveToTraining)
-                trainingWriter.println(line);
-            else
-                testingWriter.println(line);
-
-            counter++;
-        }
-
-
-        trainingWriter.close();
-        testingWriter.close();
-
-        System.out.println("No of Stocks: " + noOfStocks);
-        System.out.println("No of Days:" + noOfDays);
-
-        //pw.println(convertToLibSVM(line));
-    }
-
-    static public void splitClassificationCSV(String path, double splitPoint, String trainingPath, String testingPath) throws IOException {
-        FileReader fr = new FileReader(path);
-        BufferedReader br = new BufferedReader(fr);
-
-        File trainingFile = new File(trainingPath);
-        File testingFile = new File(testingPath);
-        PrintWriter trainingWriter = new PrintWriter(trainingFile);
-        PrintWriter testingWriter = new PrintWriter(testingFile);
-
-        String line;
-
-        while ((line = br.readLine()) != null){
-
-        }
-        //pw.println(convertToLibSVM(line));
-    }
-
-    static public void exportClassificationCSV(String stock, String path, int[] days, ProgressBar pb, double smoothPriceAlpha, boolean includeIndicators, boolean includeSentiment, boolean includeHeader, boolean ignoreNull) throws FileNotFoundException, SQLException {
+    /**
+     * Exports the data from the database into a CSV file for a given stock
+     *
+     * @param stock             Stock ticker to gather data for (e.g. AAPL for Apple Inc.)
+     * @param path              File path to save the file to
+     * @param days              List of days to calculate price rise/fall values for
+     * @param smoothPriceAlpha  Smoothing factor to apply to the price daa
+     * @param includeIndicators True if indicator values should be included, False otherwise
+     * @param includeSentiment  True if news sentiment values should be included, False otherwise
+     * @param includeHeader     True if the name of each column should be included, False otherwise
+     * @param ignoreNull        True if null values are left out, False if they should be included as a 0 value
+     * @throws FileNotFoundException Throws FileNotFoundException if the file does not exist
+     * @throws SQLException          Throws SQLException if there is an error with accessing the MySQL/MariaDB database
+     */
+    static public void exportClassificationCSV(String stock, String path, int[] days, double smoothPriceAlpha, boolean includeIndicators, boolean includeSentiment, boolean includeHeader, boolean ignoreNull) throws FileNotFoundException, SQLException {
         File file = new File(path);
         PrintWriter pw = new PrintWriter(file);
 
         ArrayList<String> dbSchema = databaseHandler.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'dailystockprices';");
 
-        String commandStart = "SELECT COALESCE(" + dbSchema.get(0) + ", 0)";
+        StringBuilder commandStart = new StringBuilder("SELECT COALESCE(" + dbSchema.get(0) + ", 0)");
         for (int i = 1; i < dbSchema.size(); i++)
-            commandStart += ",COALESCE(" + dbSchema.get(i) + ", 0)";
+            commandStart.append(",COALESCE(").append(dbSchema.get(i)).append(", 0)");
         for (String value : convertToClassificationTrainingArray(stock, commandStart + " FROM dailystockprices WHERE Symbol='" + stock + "'", " ORDER BY TradeDate ASC;", -1, days, smoothPriceAlpha, includeIndicators, includeSentiment, ignoreNull, includeHeader))
             pw.println(value);
 
         pw.close();
     }
 
+    /**
+     * Exports the data from the database into a CSV file for a given list of stocks
+     *
+     * @param stocks            List of stock tickers to gather data for (e.g. AAL, AAPL, BIIB etc.)
+     * @param path              File path to save the file to
+     * @param days              List of days to calculate price rise/fall values for
+     * @param pb                Progress Bar to visualise the progress of the export process
+     * @param smoothPriceAlpha  Smoothing factor to apply to the price daa
+     * @param includeIndicators True if indicator values should be included, False otherwise
+     * @param includeSentiment  True if news sentiment values should be included, False otherwise
+     * @param includeHeader     True if the name of each column should be included, False otherwise
+     * @param ignoreNull        True if null values are left out, False if they should be included as a 0 value
+     * @throws FileNotFoundException Throws FileNotFoundException if the file does not exist
+     * @throws SQLException          Throws SQLException if there is an error with accessing the MySQL/MariaDB database
+     */
     static public void exportClassificationCSV(ArrayList<String> stocks, String path, int[] days, ProgressBar pb, double smoothPriceAlpha, boolean includeIndicators, boolean includeSentiment, boolean includeHeader, boolean ignoreNull) throws FileNotFoundException, SQLException {
         final int t = stocks.size() - 1;
         int c = 0;
@@ -143,9 +82,9 @@ public class TrainingFileUtils {
         ArrayList<String> dbSchema = databaseHandler.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'dailystockprices';");
 
         for (String stock : stocks) {
-            String commandStart = "SELECT COALESCE(" + dbSchema.get(0) + ", 0)";
-            for(int i = 1; i < dbSchema.size(); i++)
-                commandStart += ",COALESCE(" + dbSchema.get(i) + ", 0)";
+            StringBuilder commandStart = new StringBuilder("SELECT COALESCE(" + dbSchema.get(0) + ", 0)");
+            for (int i = 1; i < dbSchema.size(); i++)
+                commandStart.append(",COALESCE(").append(dbSchema.get(i)).append(", 0)");
             for (String value : convertToClassificationTrainingArray(stock, commandStart + " FROM dailystockprices WHERE Symbol='" + stock + "'", " ORDER BY TradeDate ASC;", c, days, smoothPriceAlpha, includeIndicators, includeSentiment, ignoreNull, includeHeader && stocks.indexOf(stock) == 0))
                 pw.println(value);
 
@@ -155,6 +94,12 @@ public class TrainingFileUtils {
         pw.close();
     }
 
+    /**
+     * Converts a CSV data file to a LibSVM Format data file
+     * @param csvPath File path of the CSV file to load
+     * @param libSVMPath File path of the LibSVM file to save
+     * @throws IOException Throws IOException if either file cannot be found
+     */
     static public void exportLibSVMFile(String csvPath, String libSVMPath) throws IOException {
         Main.getController().updateCurrentTask("Converting CSV to LIBSVM file", false, false);
         FileReader fr = new FileReader(csvPath);
@@ -172,7 +117,12 @@ public class TrainingFileUtils {
         pw.close();
     }
 
-    static public String convertToLibSVM(String dataPoint){
+    /**
+     * Converts a CSV datapoint to LibSVM format
+     * @param dataPoint CSV datapoint (stock record)
+     * @return LibSVM formatted datapoint (stock record)
+     */
+    private static String convertToLibSVM(String dataPoint){
         String[] splitString = dataPoint.split(",");
         int index = 1;
 
@@ -184,6 +134,14 @@ public class TrainingFileUtils {
         return dp.toString();
     }
 
+    /**
+     * Enumerates all possible variations of the data for use in WEKA experiments to determine well-performing models
+     * @param stocks List of stocks to export files for
+     * @param pb Progress Bar to visualise the progress of the export process
+     * @param dayArray List of days to calculate price rise/fall values for
+     * @throws FileNotFoundException Throws FileNotFoundException if the file does not exist
+     * @throws SQLException Throws SQLException if there is an error with accessing the MySQL/MariaDB database
+     */
     static public void exportAllFiles(ArrayList<String> stocks, ProgressBar pb, int[] dayArray) throws FileNotFoundException, SQLException {
         //No technical indicators, no sentiment, no smoothing
         for(Integer day : dayArray)
@@ -224,46 +182,40 @@ public class TrainingFileUtils {
         }
     }
 
-    static public void exportSeparateClassificationCSV(ArrayList<String> stocks, String filePath, int[] days, ProgressBar pb) throws FileNotFoundException, SQLException {
-        final int t = stocks.size() - 1;
-        int c = 0;
-        File featureFile = new File(filePath + "Features.csv");
-        File labelFile = new File(filePath + "Labels.csv");
-        PrintWriter fpw = new PrintWriter(featureFile);
-        PrintWriter lpw = new PrintWriter(labelFile);
-
-        for (String stock : stocks) {
-            for (String value : convertToClassificationTrainingArray(stock, "SELECT * FROM dailystockprices WHERE Symbol='" + stock + "'"," ORDER BY TradeDate ASC;", c, days,0.25,true,true, false, stocks.indexOf(stock) == 0)) {
-                String[] splitString = value.split(",");
-                StringBuilder feature = new StringBuilder(splitString[0]);
-                for(int i = 1; i < splitString.length-1; i++)
-                    feature.append(",").append(splitString[i]);
-                fpw.println(feature);
-                lpw.println(splitString[splitString.length-1]);
-            }
-
-            Controller.updateProgress(++c, t, pb);
-        }
-
-        fpw.close();
-        lpw.close();
-    }
-
+    /**
+     * Resets all indicator values and smoothed prices to null
+     * @throws SQLException Throws SQLException if there is an error with accessing the MySQL/MariaDB database
+     */
     static public void resetPriceValues() throws SQLException {
         ArrayList<String> dbSchema = databaseHandler.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'dailystockprices';");
         String[] removeColumns = {"OpenPrice", "HighPrice", "LowPrice", "ClosePrice", "Symbol", "TradeDate", "TradeVolume", "PercentChange"};
 
-        for(String column:removeColumns)
+        for (String column : removeColumns)
             dbSchema.remove(column);
 
-        String command = "UPDATE dailystockprices SET " +  dbSchema.get(0) + "=null";
+        StringBuilder command = new StringBuilder("UPDATE dailystockprices SET " + dbSchema.get(0) + "=null");
 
-        for(int i = 1; i < dbSchema.size(); i++)
-            command += "," + dbSchema.get(i) + "=null";
+        for (int i = 1; i < dbSchema.size(); i++)
+            command.append(",").append(dbSchema.get(i)).append("=null");
 
         databaseHandler.executeCommand(command + ";");
     }
 
+    /**
+     * Converts data gathered from the database into a CSV array
+     * @param stock Stock to convert to CSV
+     * @param commandStart First part of the SQL command to gather data from the database
+     * @param commandEnd End part of the SQL command to gather data from the database
+     * @param index Index of the stock from the main stock list
+     * @param amountOfDaysArray List of days to calculate price rise/fall information for
+     * @param smoothPriceAlpha Smoothing factor to apply to the price daa
+     * @param includeIndicators True if indicator values should be included, False otherwise
+     * @param includeSentiments True if news sentiment values should be included, False otherwise
+     * @param includeHeader True if the name of each column should be included, False otherwise
+     * @param ignoreNull True if null values are left out, False if they should be included as a 0 value
+     * @return A CSV formatted array of records
+     * @throws SQLException Throws SQLException if there is an error with accessing the MySQL/MariaDB database
+     */
     static public ArrayList<String> convertToClassificationTrainingArray(String stock, String commandStart, String commandEnd, int index, int[] amountOfDaysArray, double smoothPriceAlpha, boolean includeIndicators, boolean includeSentiments, boolean ignoreNull, boolean includeHeader) throws SQLException {
         String[] indicators = new String[]{"SMA5", "SMA10", "SMA20", "SMA200", "EMA5", "EMA10", "EMA20", "EMA200", "MACD", "MACDSig", "MACDHist", "RSI", "ADX10", "CCI", "AD", "OBV", "StoOscSlowK", "StoOscSlowD", "WillR"};
         String [] stockData = new String[]{"OpenPrice", "HighPrice", "LowPrice", "ClosePrice", "TradeVolume", "PercentChange"};
@@ -271,15 +223,15 @@ public class TrainingFileUtils {
 
         ArrayList<String> dbSchema = databaseHandler.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'dailystockprices';");
 
-        String header = null;
+        StringBuilder header = null;
 
-        if(includeHeader) {
-            if(amountOfDaysArray.length == 1)
-                header = "Index," + stockData[0];
+        if (includeHeader) {
+            if (amountOfDaysArray.length == 1)
+                header = new StringBuilder("Index," + stockData[0]);
             else
-                header = "Index,PredictionPeriod," + stockData[0];
+                header = new StringBuilder("Index,PredictionPeriod," + stockData[0]);
             for (int i = 1; i < stockData.length; i++)
-                header += "," + stockData[i];
+                header.append(",").append(stockData[i]);
         }
 
         StringBuilder command = new StringBuilder(commandStart);
@@ -288,24 +240,23 @@ public class TrainingFileUtils {
             if (ignoreNull)
                 command.append(" AND SmoothedClosePrice IS NOT NULL");
             if(includeHeader)
-                header += ",SmoothedClosePrice";
+                header.append(",SmoothedClosePrice");
         }
 
-        if(includeIndicators) {
+        if (includeIndicators) {
             if (ignoreNull)
-            for (String column : indicators)
-                command.append(" AND ").append(column).append(" IS NOT NULL");
-            if(includeHeader)
-                for (int i = 0; i < indicators.length; i++)
-                    header += "," + indicators[i];
+                for (String column : indicators)
+                    command.append(" AND ").append(column).append(" IS NOT NULL");
+            if (includeHeader)
+                for (String indicator : indicators) header.append(",").append(indicator);
         }
 
         if(includeHeader && includeSentiments)
-            header+=",Sentiment";
+            header.append(",Sentiment");
 
-        if(includeHeader) {
-            header += ",Prediction";
-            dataPoints.add(header);
+        if (includeHeader) {
+            header.append(",Prediction");
+            dataPoints.add(header.toString());
         }
 
         ArrayList<String> priceValues = databaseHandler.executeQuery(command + commandEnd);
@@ -343,10 +294,10 @@ public class TrainingFileUtils {
 
             for (int i = 0; i < futurePrices.length; i++) {
                 StringBuilder dataPoint = new StringBuilder();
-                if(index >= 0)
-                    dataPoint.append(String.valueOf(index) + ",");
-                if(amountOfDaysArray.length > 1)
-                    dataPoint.append(String.valueOf(amountOfDays) + ",");
+                if (index >= 0)
+                    dataPoint.append(String.valueOf(index)).append(",");
+                if (amountOfDaysArray.length > 1)
+                    dataPoint.append(String.valueOf(amountOfDays)).append(",");
                 String[] splitString = priceValues.get(i).split(",");
 
                 dataPoint.append(splitString[dbSchema.indexOf(stockData[0])]);
@@ -356,16 +307,16 @@ public class TrainingFileUtils {
                 if(smoothPriceAlpha != 1)
                     dataPoint.append(",").append(splitString[dbSchema.indexOf("SmoothedClosePrice")]);
 
-                if(includeIndicators)
-                    for(String indicator : indicators)
-                            dataPoint.append(",").append(splitString[dbSchema.indexOf(indicator)]);
+                if (includeIndicators)
+                    for (String indicator : indicators)
+                        dataPoint.append(",").append(splitString[dbSchema.indexOf(indicator)]);
 
                 if(includeSentiments)
                     dataPoint.append(",").append(sentiments[i]);
 
                 dataPoint.append(",").append(String.valueOf((futurePrices[i] - currentPrices[i]) >= 0 ? 1 : 0));
 
-                if(includeHeader && dataPoints.size() == 1 && header.split(",").length != dataPoint.toString().split(",").length)
+                if (includeHeader && dataPoints.size() == 1 && header.toString().split(",").length != dataPoint.toString().split(",").length)
                     Main.getController().updateCurrentTask("Header/Datapoint Size Mismatch!", true, false);
 
                 dataPoints.add(dataPoint.toString());
